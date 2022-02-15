d6b9193526fb9a3ece8914ed5cf02442b2fa6235
melabu <analia.drago@hotmail.com>
2022-02-15 13:30:13 -0300

    displaying the components depending on the URL

--- app/Main.js
+++ app/Main.js
@@ -1,23 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //My Components
 import Header from "./Components/Header";
 import HomeGuest from "./components/HomeGuest";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
 
 function Main() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
+      <Routes>
+        <Route path="/" element={<HomeGuest />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
 ReactDOM.render(<Main />, document.querySelector("#app"));
 
 if (module.hot) {
-  module.hot.accept();
+  module.hot.accept(); //autorefresh the browser
 }
